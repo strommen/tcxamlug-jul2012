f33f15360a54d1083f43c15b8d0eee655dc7b8c5
Joe Strommen <joe.strommen@gmail.com>
2012-06-28 23:00:02 -0500

    Added IScrollInfo stuff (but didn't declare it)

--- WPF Performance Demos/MapLayoutPanel.cs
+++ WPF Performance Demos/MapLayoutPanel.cs
@@ -5,10 +5,11 @@ using System.Text;
 using System.Windows.Controls;
 using System.Windows;
 using System.Windows.Controls.Primitives;
+using System.Windows.Media;
 
 namespace WPF_Performance_Demos
 {
-	public class MapLayoutPanel : Panel
+	public class MapLayoutPanel : Panel//, IScrollInfo
 	{
 		#region Attached Properties for Lat/Long
 
@@ -92,6 +93,8 @@ namespace WPF_Performance_Demos
 
 		#endregion
 
+		#region Layout Methods
+
 		protected override Size MeasureOverride(Size availableSize)
 		{
 			if (this.InternalChildren.Count == 0) return new Size(0, 0);
@@ -116,13 +119,18 @@ namespace WPF_Performance_Demos
 				maxY = Math.Max(maxY, location.Y);
 			}
 
+			_Extent.Width = maxX - minX;
+			_Extent.Height = maxY - minY;
+
 			panelCenter = new Point((minX + maxX) / 2, (minY + maxY) / 2);
 
-			return new Size(Math.Min(availableSize.Width, maxX - minX), Math.Min(availableSize.Height, maxY - minY));
+			return new Size(Math.Min(availableSize.Width, _Extent.Width), Math.Min(availableSize.Height, _Extent.Height));
 		}
 
 		protected override Size ArrangeOverride(Size finalSize)
 		{
+			_Viewport = finalSize;
+
 			foreach (UIElement child in this.InternalChildren)
 			{
 				var lat = GetLatitude(child);
@@ -133,13 +141,133 @@ namespace WPF_Performance_Demos
 				var width = child.DesiredSize.Width;
 				var height = child.DesiredSize.Height;
 				child.Arrange(new Rect(
-					location.X - width / 2,
-					location.Y - height / 2,
+					_Offset.X + location.X - width / 2,
+					_Offset.Y + location.Y - height / 2,
 					width,
 					height));
 			}
 
 			return finalSize;
 		}
+
+		#endregion
+
+		#region IScrollInfo Implementation
+
+		public ScrollViewer ScrollOwner { get; set; }
+
+		public bool CanHorizontallyScroll { get; set; }
+
+		public bool CanVerticallyScroll { get; set; }
+
+		#region Movement Methods
+
+		private const double LineSize = 16;
+		private const double WheelSize = 3 * LineSize;
+
+		public void LineDown() { SetVerticalOffset(VerticalOffset + LineSize); }
+
+		public void LineUp() { SetVerticalOffset(VerticalOffset - LineSize); }
+
+		public void LineLeft() { SetHorizontalOffset(HorizontalOffset - LineSize); }
+
+		public void LineRight() { SetHorizontalOffset(HorizontalOffset + LineSize); }
+
+		public void MouseWheelDown() { SetVerticalOffset(VerticalOffset + WheelSize); }
+
+		public void MouseWheelUp() { SetVerticalOffset(VerticalOffset - WheelSize); }
+
+		public void MouseWheelLeft() { SetHorizontalOffset(HorizontalOffset - WheelSize); }
+
+		public void MouseWheelRight() { SetHorizontalOffset(HorizontalOffset + WheelSize); }
+
+		public void PageDown() { SetVerticalOffset(VerticalOffset + ViewportHeight); }
+
+		public void PageUp() { SetVerticalOffset(VerticalOffset - ViewportHeight); }
+
+		public void PageLeft() { SetHorizontalOffset(HorizontalOffset - ViewportWidth); }
+
+		public void PageRight() { SetHorizontalOffset(HorizontalOffset + ViewportWidth); }
+
+		#endregion
+
+		#region Display Properties
+
+		private Vector _Offset;
+		private Size _Extent;
+		private Size _Viewport;
+
+		public double ExtentHeight
+		{
+			get { return _Extent.Height; }
+		}
+
+		public double ExtentWidth
+		{
+			get { return _Extent.Width; }
+		}
+
+		public double HorizontalOffset
+		{
+			get { return _Offset.X; }
+		}
+
+		public double VerticalOffset
+		{
+			get { return _Offset.Y; }
+		}
+
+		public double ViewportHeight
+		{
+			get { return _Viewport.Height; }
+		}
+
+		public double ViewportWidth
+		{
+			get { return _Viewport.Width; }
+		}
+
+		#endregion
+
+		#region Offset Changes
+
+		public void SetHorizontalOffset(double offset)
+		{
+			if (offset < 0)
+				offset = 0;
+
+			if (offset + ViewportWidth > ExtentWidth)
+				offset = ViewportWidth - ExtentWidth;
+
+			if (offset != _Offset.X)
+			{
+				_Offset.X = offset;
+				InvalidateArrange();
+			}
+		}
+
+		public void SetVerticalOffset(double offset)
+		{
+			if (offset < 0)
+				offset = 0;
+
+			if (offset + ViewportHeight > ExtentHeight)
+				offset = ExtentHeight - ViewportHeight;
+
+			if (offset != _Offset.Y)
+			{
+				_Offset.Y = offset;
+				InvalidateArrange();
+			}
+		}
+
+		#endregion
+
+		public Rect MakeVisible(Visual visual, Rect rectangle)
+		{
+			return Rect.Empty;
+		}
+
+		#endregion
 	}
 }
